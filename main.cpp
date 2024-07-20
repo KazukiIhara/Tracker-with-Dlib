f5a6f104385edf44f7a04f792925523fe7e3c957
IharaKazuki <kazuto3996@gmail.com>
2024-07-21 08:38:06 +0900

    おおよそ目の中心を検知することに成功

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #define NOMINMAX
+#include <opencv2/opencv.hpp>
 #include <dlib/opencv.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp> 
@@ -31,6 +32,13 @@ int main() {
 	deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
 	cv::Mat temp;
 
+	// Haar Cascadeの読み込み
+	cv::CascadeClassifier eye_cascade;
+	if (!eye_cascade.load("haarcascade_eye.xml")) {
+		std::cout << "Error loading haarcascade_eye.xml" << std::endl;
+		return -1;
+	}
+
 	// メインループ
 	while (!win.is_closed()) {
 		// 1フレーム分の画像を受け取る箱
@@ -62,23 +70,68 @@ int main() {
 				dlib::draw_solid_circle(img, shapes[i].part(j), 2, dlib::rgb_pixel(0, 255, 0));
 			}
 
+
 			// 目の部分を切り取る
 			int margin = 15; // 追加するマージン（ピクセル単位）
-			cv::Rect left_eye(std::max(0, static_cast<int>(shapes[i].part(42).x()) - margin), std::max(0, static_cast<int>(shapes[i].part(43).y()) - margin), std::min(temp.cols, static_cast<int>(shapes[i].part(45).x()) + margin) - std::max(0, static_cast<int>(shapes[i].part(42).x()) - margin), std::min(temp.rows, static_cast<int>(shapes[i].part(46).y()) + margin) - std::max(0, static_cast<int>(shapes[i].part(43).y()) - margin));
-			cv::Rect right_eye(std::max(0, static_cast<int>(shapes[i].part(36).x()) - margin), std::max(0, static_cast<int>(shapes[i].part(37).y()) - margin), std::min(temp.cols, static_cast<int>(shapes[i].part(40).x()) + margin) - std::max(0, static_cast<int>(shapes[i].part(36).x()) - margin), std::min(temp.rows, static_cast<int>(shapes[i].part(39).y()) + margin) - std::max(0, static_cast<int>(shapes[i].part(37).y()) - margin));			cv::Mat left_eye_img = temp(left_eye);
+			cv::Rect left_eye(std::max(0, static_cast<int>(shapes[i].part(42).x()) - margin), std::max(0, static_cast<int>(shapes[i].part(43).y()) - margin), std::min(temp.cols, static_cast<int>(shapes[i].part(45).x()) + margin) - std::max(0, static_cast<int>(shapes[i].part(42).x()) - margin), std::min(temp.rows, static_cast<int>(shapes[i].part(47).y()) + margin) - std::max(0, static_cast<int>(shapes[i].part(43).y()) - margin));
+			cv::Rect right_eye(std::max(0, static_cast<int>(shapes[i].part(36).x()) - margin), std::max(0, static_cast<int>(shapes[i].part(37).y()) - margin), std::min(temp.cols, static_cast<int>(shapes[i].part(39).x()) + margin) - std::max(0, static_cast<int>(shapes[i].part(36).x()) - margin), std::min(temp.rows, static_cast<int>(shapes[i].part(41).y()) + margin) - std::max(0, static_cast<int>(shapes[i].part(37).y()) - margin));			cv::Mat left_eye_img = temp(left_eye);
 			cv::Mat right_eye_img = temp(right_eye);
 
 			// 画像をリサイズ
 			cv::resize(left_eye_img, left_eye_img, cv::Size(300, 300));
 			cv::resize(right_eye_img, right_eye_img, cv::Size(300, 300));
 
-			cv::imshow("Left Eye", left_eye_img);
-			cv::imshow("Right Eye", right_eye_img);
+			// グレースケールに変換
+			cv::Mat gray_left_eye, gray_right_eye;
+			cv::cvtColor(left_eye_img, gray_left_eye, cv::COLOR_BGR2GRAY);
+			cv::cvtColor(right_eye_img, gray_right_eye, cv::COLOR_BGR2GRAY);
+
+			// ガウシアンブラーを適用
+			cv::GaussianBlur(gray_left_eye, gray_left_eye, cv::Size(7, 7), 0);
+			cv::GaussianBlur(gray_right_eye, gray_right_eye, cv::Size(7, 7), 0);
+
+			// Otsuの二値化
+			cv::Mat th_left_eye, th_right_eye;
+			double thresh_left_eye = cv::threshold(gray_left_eye, th_left_eye, 0, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
+			double thresh_right_eye = cv::threshold(gray_right_eye, th_right_eye, 0, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
+
+			// 輪郭を検出
+			std::vector<std::vector<cv::Point>> contours_left_eye, contours_right_eye;
+			cv::findContours(th_left_eye, contours_left_eye, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+			cv::findContours(th_right_eye, contours_right_eye, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+			// 最大の輪郭を見つける
+			std::vector<cv::Point> cnt_left_eye = contours_left_eye[0];
+			for (const auto& c : contours_left_eye) {
+				if (cnt_left_eye.size() < c.size()) {
+					cnt_left_eye = c;
+				}
+			}
+			std::vector<cv::Point> cnt_right_eye = contours_right_eye[0];
+			for (const auto& c : contours_right_eye) {
+				if (cnt_right_eye.size() < c.size()) {
+					cnt_right_eye = c;
+				}
+			}
+
+			// 楕円フィッティング
+			if (cnt_left_eye.size() > 5) {
+				cv::RotatedRect ellipse = cv::fitEllipse(cnt_left_eye);
+				cv::Point2f center = ellipse.center;
+				// 重心に円を描画
+				cv::circle(left_eye_img, center, 5, cv::Scalar(0, 255, 255), -1);
+			}
+
+			if (cnt_right_eye.size() > 5) {
+				cv::RotatedRect ellipse = cv::fitEllipse(cnt_right_eye);
+				cv::Point2f center = ellipse.center;
+				// 重心に円を描画
+				cv::circle(right_eye_img, center, 5, cv::Scalar(0, 255, 255), -1);
+			}
 
 			cv::imshow("Left Eye", left_eye_img);
 			cv::imshow("Right Eye", right_eye_img);
 		}
-
 
 		// ランドマーク付きの画像を出力
 		win.set_image(img);
